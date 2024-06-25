79ed1a08db3a741162cf7079c00c1ec41c9dbc89
Divyam Goel <divyamairu200@gmail.com>
2024-06-25 19:28:34 +0530

    first changes

--- Firebase/ClientApp.mjs
+++ Firebase/ClientApp.mjs
@@ -1,3 +1,33 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {
+//     getAuth,
+// } from "firebase/auth";
+// import {
+//     getFirestore,
+// } from "firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: import.meta.env.VITE_FIREBASE_APP_ID,
+//     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+
+// export {
+//     auth,
+//     db,
+//     firebaseConfig
+// };
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -9,14 +39,14 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+    apiKey: "AIzaSyBfV6pOdATXg3M4QaCBpcz7GMDwE2T71z8",
+    authDomain: "resource-management-235e4.firebaseapp.com",
+    projectId: "resource-management-235e4",
+    storageBucket: "resource-management-235e4.appspot.com",
+    messagingSenderId: "1040053510153",
+    appId: "1:1040053510153:web:54fb81f384bcfdd24f98d8",
+    measurementId: "G-S3VVYRSWXT"
+  };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
